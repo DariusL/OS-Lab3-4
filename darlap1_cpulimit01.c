cc26a6738ccfe6a4fdac055972ec3fe46e14bd45
LAPŪNAS Darius IF-1/8 2013-02-05 <darlap1@os.(none)>
2013-04-22 18:22:39 +0300

    added cpulimit

---
+++ darlap1_cpulimit01.c
@@ -0,0 +1,13 @@
+/*Darius Lapunas IF-1/8 darlap1*/
+/*darlap1_cpulimit01.c*/
+
+#include <aio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+
